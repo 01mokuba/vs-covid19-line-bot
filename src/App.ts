6960c34d0282035868cf42e2f8d886e18707e001
yokinist <ikd618@gmail.com>
2020-04-06 00:22:02 +0900

    :sparkles: Fetch support data from API

--- src/App.ts
+++ src/App.ts
@@ -1,45 +1,18 @@
+import { Support } from './typings'
 // Checking whether git and clasp are working correctly
 const COVID_19 = 'https://vscovid19.code4japan.org/';
 
 const CHANNEL_ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty("CHANNEL_ACCESS_TOKEN");
 const LINE_ENDPOINT_REPLY = 'https://api.line.me/v2/bot/message/reply';
 const LINE_ENDPOINT_MULTICAST = 'https://api.line.me/v2/bot/message/multicast';
+const API_URL = "https://app.sabae.cc/api/googlespreadsheet.json?key=2PACX-1vSFMNp5HcRNOF5MrAujEUWR1dIoX2mncMEWTbPlVAaJqKWiq831-6gnCyI7n_G8YfPqNQXrfwyVjyHL&fbclid=IwAR1COPWKIjz5rH-nHD4Raned5-_tIxRCcDpFIfTplxqkGbjkh5ifKjOopOI"
 
-const SHEET1_ID = PropertiesService.getScriptProperties().getProperty("SHEET1_ID");
-const SHEET1_NAME = 'data';
+// support data
+const res = UrlFetchApp.fetch(API_URL);
+const supports: Support[] = JSON.parse(res.getContentText());
+// user data
 const SHEET2_ID = PropertiesService.getScriptProperties().getProperty("SHEET2_ID");
 const SHEET2_NAME = 'users';
-
-const SHEET1 = SpreadsheetApp.openById(SHEET1_ID).getSheetByName(SHEET1_NAME);
-const SHEET1_DATA = SHEET1.getRange(2,1,SHEET1.getLastRow()-1,SHEET1.getLastColumn()-1).getValues();
-
-let sheet1DataArrayOfDicts = [];
-for (var i = 0; i < SHEET1_DATA.length; i++) {
-  sheet1DataArrayOfDicts.push([]);
-  sheet1DataArrayOfDicts[i]["id"] = SHEET1_DATA[i][0];
-  sheet1DataArrayOfDicts[i]["addDate"] = SHEET1_DATA[i][1];
-  sheet1DataArrayOfDicts[i]["field"] = SHEET1_DATA[i][2];
-  sheet1DataArrayOfDicts[i]["category"] = SHEET1_DATA[i][3];
-  sheet1DataArrayOfDicts[i]["keyword"] = SHEET1_DATA[i][4];
-  sheet1DataArrayOfDicts[i]["serviceName"] = SHEET1_DATA[i][5];
-  sheet1DataArrayOfDicts[i]["target"] = SHEET1_DATA[i][6];
-  sheet1DataArrayOfDicts[i]["startDate"] = SHEET1_DATA[i][7];
-  sheet1DataArrayOfDicts[i]["endDate"] = SHEET1_DATA[i][8];
-  sheet1DataArrayOfDicts[i]["dateNotes"] = SHEET1_DATA[i][9];
-  sheet1DataArrayOfDicts[i]["price"] = SHEET1_DATA[i][10];
-  sheet1DataArrayOfDicts[i]["support"] = SHEET1_DATA[i][11];
-  sheet1DataArrayOfDicts[i]["orgName"] = SHEET1_DATA[i][12];
-  sheet1DataArrayOfDicts[i]["details"] = SHEET1_DATA[i][13];
-  sheet1DataArrayOfDicts[i]["releaseDate"] = SHEET1_DATA[i][14];
-  sheet1DataArrayOfDicts[i]["url"] = SHEET1_DATA[i][15];
-  sheet1DataArrayOfDicts[i]["source"] = SHEET1_DATA[i][16];
-}
-
-const SHEET1_DATA_ARRAY_OF_DICTS = sheet1DataArrayOfDicts;
-const SHEET1_DATA_ARRAY_OF_DICTS_STRING = SHEET1_DATA.map(row => {
-  return row.join(',');
-});
-
 const SHEET2 = SpreadsheetApp.openById(SHEET2_ID).getSheetByName(SHEET2_NAME);
 const SHEET2_DATA = SHEET2.getDataRange().getValues();
 
@@ -66,9 +39,6 @@ function doPost(e) {
           break;
         case "支援を検索したい":
           replyAboutHowToUseSearch(replyToken);
-          break;
-        case "新着情報を知りたい":
-          replyTodayData(replyToken);
           break;
         case "感想や意見を送りたい":
           replyFormUrl(replyToken);
@@ -112,29 +82,13 @@ function replyAboutHowToUseSearch(replyToken) {
   fetchLineEndpointReply(replyToken, messages);
 }
 
-function replyTodayData(replyToken) {
-  const results = getTodayData();
-  const resultsCount = results.length;
-  if (resultsCount !== 0) {
-    const messages = [`昨日と今日は${resultsCount}件の新着情報がありました`];
-    addMessages(results, resultsCount, messages);
-    if (resultsCount > 3) {
-      messages.push(`続きはこちらから！\n${COVID_19}`);
-    }
-    fetchLineEndpointReply(replyToken, messages);
-  } else {
-    const messages = [`昨日と今日は新着情報がありません`];
-    fetchLineEndpointReply(replyToken, messages);
-  }
-}
-
 function replyFormUrl(replyToken) {
   const messages = [`以下のURLからご感想・ご意見をお寄せください！開発の参考にさせていただきます`,`https://forms.gle/GffWz4bJwDPHaGMTA`];
   fetchLineEndpointReply(replyToken, messages);
 }
 
 function replyMessages(replyToken, postMessage) {
-  const results = getData(postMessage);
+  const results = searchSupports(postMessage);
   const resultsCount = results.length;
   const messages = [`${resultsCount}件がヒットしました`];
   addMessages(results, resultsCount, messages);
@@ -186,14 +140,14 @@ function formatMessages(results, resultsCount, messages, limit) {
   }
 }
 
-function getData(word) {
-  let results = [];
-  for (var i = 0; i < SHEET1_DATA_ARRAY_OF_DICTS_STRING.length; i++) {
-    if (SHEET1_DATA_ARRAY_OF_DICTS_STRING[i].indexOf(word) !== -1) {
-      results.push(SHEET1_DATA_ARRAY_OF_DICTS[i]);
-    }
-  }
-  return results;
+function searchSupports(word: string): Support[] {
+  const filteredByWordSupports = supports.filter(
+    support =>
+      support['サービス名称'].includes(word) ||
+      support['詳細'].includes(word) ||
+      support['企業等'].includes(word),
+  );
+  return filteredByWordSupports
 }
 
 function dateToString(date) {
@@ -205,7 +159,7 @@ function dateToString(date) {
 }
 
 function multicast() {
-  let results = getTodayData();
+  let results = supports;
   let resultsCount = results.length;
   if (resultsCount !== 0) {
     let messages = [`昨日と今日は${resultsCount}件の新着情報がありました`];
@@ -215,22 +169,6 @@ function multicast() {
     }
     fetchLineEndpointMulticast(messages)
   }
-}
-
-function getTodayData() {
-  let results = [];
-  let now = new Date();
-  let todayDate = dateToString(now);
-  let yesterdayDate = dateToString(new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1));
-  for (var i = 0; i < SHEET1_DATA_ARRAY_OF_DICTS.length; i++) {
-    if (SHEET1_DATA_ARRAY_OF_DICTS[i]["addDate"] !== "") {
-      let addDate = dateToString(SHEET1_DATA_ARRAY_OF_DICTS[i]["addDate"]);
-      if (addDate == todayDate || addDate == yesterdayDate) {
-        results.push(SHEET1_DATA_ARRAY_OF_DICTS[i]);
-      }
-    }
-  }
-  return results;
 }
 
 function fetchLineEndpointMulticast(messages) {
